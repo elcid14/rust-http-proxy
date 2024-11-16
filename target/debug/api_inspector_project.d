e584c771d690b7409c949249cbe79d85fc57fbe6
elcid14 <hickscw2014@gmail.com>
2024-11-16 06:37:47 -0500

    update: first commit

---
+++ target/debug/api_inspector_project.d
@@ -0,0 +1,1 @@
+/home/hicks/RustProjects/api_inspector_project/target/debug/api_inspector_project: /home/hicks/RustProjects/api_inspector_project/src/main.rs
---
+++ target/debug/deps/api_inspector_project-ea8cbf509af5c84a.d
@@ -0,0 +1,5 @@
+/home/hicks/RustProjects/api_inspector_project/target/debug/deps/api_inspector_project-ea8cbf509af5c84a: src/main.rs
+
+/home/hicks/RustProjects/api_inspector_project/target/debug/deps/api_inspector_project-ea8cbf509af5c84a.d: src/main.rs
+
+src/main.rs:
